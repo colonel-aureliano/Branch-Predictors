aa236d308e0956abfa1ba6acb926bf0a1634cd37
angelac345 <angelachao345@gmail.com>
2023-11-26 18:17:57 -0500

    global branch predictor testing

--- sim/lab4_branch/BranchGlobal.v
+++ sim/lab4_branch/BranchGlobal.v
@@ -8,7 +8,8 @@
 `include "vc/mem-msgs.v"
 `include "vc/queues.v"
 `include "vc/trace.v"
-
+`include "vc/muxes.v"
+`include "vc/regs.v" 
 
 module lab4_branch_BranchGlobal
 #(
@@ -24,6 +25,147 @@ module lab4_branch_BranchGlobal
 
 );
 
+  logic glob_reg_en; 
+  logic glob_mux_sel; 
+  logic pht_wen; 
+  logic [1:0] pht_wdata; 
+  logic [1:0] rdata; 
+
+  lab4_branch_BranchGlobal_DPath #(PHT_size) dpath
+  (
+    .clk       (clk), 
+    .reset     (reset), 
+    .update_val (update_val),
+    .pht_wen   (pht_wen), 
+    .pht_wdata (pht_wdata), 
+    .glob_reg_en (glob_reg_en), 
+    .glob_mux_sel (glob_mux_sel), 
+    .PC        (PC), 
+    .rdata     (rdata), 
+    .prediction(prediction)   
+  ); 
+
+  lab4_branch_BranchGlobal_Ctrl ctrl 
+  (
+    .clk        (clk), 
+    .reset      (reset), 
+    .rdata      (rdata), 
+    .update_en  (update_en), 
+    .update_val (update_val), 
+    .pht_wen    (pht_wen), 
+    .pht_wdata  (pht_wdata), 
+    .glob_reg_en (glob_reg_en), 
+    .glob_mux_sel (glob_mux_sel)
+  );
+
+
+endmodule
+
+module lab4_branch_BranchGlobal_Ctrl 
+(
+  input  logic       clk, 
+  input  logic       reset, 
+
+  input  logic [1:0] rdata, 
+
+  input  logic       update_en, 
+  input  logic       update_val, 
+
+  output logic       pht_wen, 
+  output logic [1:0] pht_wdata, 
+  output logic       glob_reg_en, 
+  output logic       glob_mux_sel
+);
+  
+  always_comb begin 
+    if ( update_en ) begin       
+      casez ( rdata ) 
+        2'b00:   if ( update_val ) pht_wdata = 2'b1; 
+                 else pht_wdata = 2'b0; 
+        2'b11:   if ( update_val ) pht_wdata = 2'b11; 
+                 else pht_wdata = 2'b10; 
+        default: if ( update_val ) pht_wdata = rdata + 1; 
+                 else pht_wdata = rdata - 1; 
+      endcase
+    end 
+    else begin 
+      pht_wdata = 2'hx; 
+    end 
+  end
+
+  assign pht_wen = update_en; 
+  assign glob_mux_sel = !reset; 
+  assign glob_reg_en = reset || update_en; 
+  
+endmodule
+
+
+module lab4_branch_BranchGlobal_DPath 
+#(
+  parameter PHT_size = 2048, 
+
+  parameter c_addr_nbits  = $clog2(PHT_size)
+)
+(
+  input logic         clk, 
+  input logic         reset, 
+  input logic         update_val,
+
+  input logic         pht_wen, 
+  input logic  [ 1:0] pht_wdata, 
+  input logic         glob_reg_en, 
+  input logic         glob_mux_sel, 
+
+  input logic  [31:0] PC, 
+
+  output logic [ 1:0] rdata, 
+  output logic        prediction 
+
+);
+  
+  localparam z = 0; 
+
+  logic [c_addr_nbits-1:0] next_glob; 
+  logic [c_addr_nbits-1:0] glob_mux_out; 
+  
+  vc_Mux2 #(c_addr_nbits) glob_mux 
+  (
+    .in0 (z), 
+    .in1 (next_glob), 
+    .sel (glob_mux_sel), 
+    .out (glob_mux_out)
+  ); 
+
+  logic [c_addr_nbits-1:0] index; 
+  vc_EnReg #(c_addr_nbits) glob_reg 
+  (
+    .clk (clk), 
+    .reset (reset), 
+    .d    ( glob_mux_out ), 
+    .q    ( index ), 
+    .en   ( glob_reg_en ) 
+  );
+
+  logic [c_addr_nbits-2:0] extender; 
+  assign extender = 0; 
+
+  assign next_glob = ( index << 1 ) + {extender, update_val}; 
+
+  
+  vc_Regfile_1r1w #(2, PHT_size) pht
+  (
+    .clk(clk), 
+    .reset(reset), 
+
+    .read_addr(index),
+    .read_data(rdata),
+    .write_en(pht_wen),
+    .write_addr(index),
+    .write_data(pht_wdata)
+  );
+
+
+  assign prediction = rdata[1]; 
 
 endmodule
 
---
+++ sim/lab4_branch/utb_BranchGlobal.v
@@ -0,0 +1,238 @@
+//========================================================================
+// utb_BranchGlobal
+//========================================================================
+// A basic Verilog unit test bench for the Global Branch Predictor Design module
+
+`default_nettype none
+`timescale 1ps/1ps
+
+
+`include "BranchGlobal.v"
+`include "vc/trace.v"
+`include "vc/regfiles.v"
+//------------------------------------------------------------------------
+// Top-level module
+//------------------------------------------------------------------------
+
+module top(  input logic clk, input logic linetrace );
+
+    logic         reset;
+    logic         update_en;
+    logic         update_val;
+    logic  [31:0] PC;
+    logic         prediction;
+    //----------------------------------------------------------------------
+    // Module instantiations
+    //----------------------------------------------------------------------
+    
+    // Instantiate the processor datapath
+    lab4_branch_BranchGlobal #(16) DUT
+    ( 
+        .*
+    ); 
+
+    //----------------------------------------------------------------------
+    // Run the Test Bench
+    //----------------------------------------------------------------------
+
+    logic [3:0] place_holder;
+    initial begin
+
+        $display("Start of Testbench");
+        // Initalize all the signal inital values.
+        reset = 1; 
+        update_en = 0; 
+        update_val = 0; 
+        PC = 0; 
+        @(negedge clk); 
+        
+        reset = 0; 
+        @(negedge clk); 
+        
+        //----------------------------------------------------------------------
+        // Predict 0 on first round
+        //----------------------------------------------------------------------
+
+        PC = {20'b0, 10'd131, 2'd0}; 
+
+        #0.02; 
+        assertion( "initial should predict 0", 1'b0, prediction); 
+        @(negedge clk); 
+
+        //----------------------------------------------------------------------
+        // Update branch history 1 time 
+        //----------------------------------------------------------------------
+        
+        $display("updating %d", 0); 
+        update_val = 1; 
+        update_en = 1; 
+        
+        @(negedge clk); 
+        update_en = 0;
+
+        assertion4("global history after update ", 4'b01, DUT.dpath.index); 
+        assertion2("rfile after update", 2'b01, DUT.dpath.pht.rfile[0]); 
+        @(negedge clk); 
+
+        //----------------------------------------------------------------------
+        // Update branch history 4 more times with 0
+        //----------------------------------------------------------------------
+
+        for (integer i = 1; i < 5; i++) begin 
+            $display("iteration: %d", i);
+            update_val = 0; 
+            update_en = 1; 
+            
+            @(negedge clk); 
+            update_en = 0;
+
+            assertion4("global history after update ", 4'b1 << i, DUT.dpath.index); 
+            assertion2("rfile after update", 2'b00, DUT.dpath.pht.rfile[4'b1 << (i-1)]); 
+            @(negedge clk); 
+        end 
+        assertion("prediction with history 0000", 1'b0, prediction);
+
+        //----------------------------------------------------------------------
+        // Update branch history 1 time, update pht[0] from 01 to 10
+        //----------------------------------------------------------------------
+        
+        $display("updating %d", 0); 
+        update_val = 1; 
+        update_en = 1; 
+        
+        @(negedge clk); 
+        update_en = 0;
+
+        assertion4("global history after update ", 4'b01, DUT.dpath.index); 
+        assertion2("rfile after update", 2'b10, DUT.dpath.pht.rfile[0]); 
+        @(negedge clk); 
+
+        //----------------------------------------------------------------------
+        // Update branch history 4 more times with 0
+        //----------------------------------------------------------------------
+
+        for (integer i = 1; i < 5; i++) begin 
+            $display("iteration: %d", i);
+            update_val = 0; 
+            update_en = 1; 
+            
+            @(negedge clk); 
+            update_en = 0;
+
+            assertion4("global history after update ", 4'b1 << i, DUT.dpath.index); 
+            assertion2("rfile after update", 2'b00, DUT.dpath.pht.rfile[4'b1 << (i-1)]); 
+            @(negedge clk); 
+        end 
+
+        assertion("prediction: ", 1'b1, prediction);
+
+        //----------------------------------------------------------------------
+        // Update branch history 1 time, update pht[0] from 10 to 11
+        //----------------------------------------------------------------------
+        
+        $display("updating %d", 0); 
+        update_val = 1; 
+        update_en = 1; 
+        
+        @(negedge clk); 
+        update_en = 0;
+
+        assertion4("global history after update ", 4'b01, DUT.dpath.index); 
+        assertion2("rfile after update", 2'b11, DUT.dpath.pht.rfile[0]); 
+        @(negedge clk); 
+
+        //----------------------------------------------------------------------
+        // Update branch history 4 more times with 1
+        //----------------------------------------------------------------------
+        
+        assign place_holder = 1; 
+        for (integer i = 1; i < 5; i++) begin 
+            $display("iteration: %d", i);
+            update_val = 1; 
+            update_en = 1; 
+            
+            @(negedge clk); 
+            update_en = 0;
+
+            assertion4("global history after update ", (place_holder << 1) + 1, DUT.dpath.index); 
+            assertion2("rfile after update", 2'b01, DUT.dpath.pht.rfile[place_holder]); 
+            assertion("small prediction", 1'b0, prediction); 
+
+            assign place_holder = (place_holder << 1) + 1;
+            @(negedge clk); 
+        end 
+
+        assertion("prediction at history 1111", 1'b0, prediction);
+
+        //----------------------------------------------------------------------
+        // Update branch history 4 more times with 1, history remain in 1111, update from 01 to 11
+        //----------------------------------------------------------------------
+        
+        assign place_holder = 4'b1;  // should hold the expected index value
+        for (integer i = 1; i < 5; i++) begin 
+            $display("iteration: %d", i);
+            update_val = 1; 
+            update_en = 1; 
+            
+            @(negedge clk); 
+            update_en = 0;
+
+            if ( place_holder < 4'b11 ) place_holder = place_holder + 1; 
+
+            assertion4("global history after update ", 4'b1111, DUT.dpath.index); 
+            assertion2("rfile after update", place_holder[1:0], DUT.dpath.pht.rfile[15]); 
+            assertion("small prediction", place_holder[1], prediction); 
+
+            
+            @(negedge clk); 
+        end 
+
+        assertion("prediction at history 1111", 1'b1, prediction);
+
+        #20; 
+        $finish();
+    end
+  
+    task assertion( string varname, [0:0] expected, [0:0] actual ); 
+        begin 
+            assert(expected == actual) begin
+                $display("%s is correct.  Expected: %h, Actual: %h", varname, expected, actual); pass();
+            end else begin
+                $display("%s is incorrect.  Expected: %h, Actual: %h", varname, expected, actual); fail(); 
+            end 
+        end
+    endtask
+
+    task assertion2( string varname, [1:0] expected, [1:0] actual ); 
+        begin 
+            assert(expected == actual) begin
+                $display("%s is correct.  Expected: %b, Actual: %b", varname, expected, actual); pass();
+            end else begin
+                $display("%s is incorrect.  Expected: %b, Actual: %b", varname, expected, actual); fail(); 
+            end 
+        end
+    endtask
+
+    task assertion4( string varname, [3:0] expected, [3:0] actual ); 
+        begin 
+            assert(expected == actual) begin
+                $display("%s is correct.  Expected: %b, Actual: %b", varname, expected, actual); pass();
+            end else begin
+                $display("%s is incorrect.  Expected: %b, Actual: %b", varname, expected, actual); fail(); 
+            end 
+        end
+    endtask
+
+    logic clock_counter; 
+    initial begin 
+        clock_counter = 0; 
+        for (integer i = 0; i < 20000; i++) begin 
+            @(posedge clk);
+            clock_counter = !clock_counter; 
+        end
+
+        $display("test time exceeded, Terminating"); 
+        $finish();
+    end
+
+endmodule
